9fe3e6e21bfe12057ece2ddbc429984d1ed2d8ab
Tomas Breuckmann <tomas.yoga@yahoo.com>
2021-12-28 00:03:36 -0300

    projeto meme generator - criterio 4 e 6 - npm test ok

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+const fire = document.getElementById('fire')
+const memeImageContainer = document.getElementById('meme-image-container')
+
 function memeText(){
     let texto=document.getElementById('text-input')
     let containerText = document.getElementById('meme-text')
     containerText.innerText=texto.value
 }
-memeText()
 
-const x=document.querySelector('#meme-insert')
+function bordaFire(){
+    memeImageContainer.style.border='3px dashed red'
+}
+function bordaWater(){
+    memeImageContainer.style.border='5px double blue'
+}
+function bordaEarth(){
+    memeImageContainer.style.border='6px groove green'
+}
