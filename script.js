4e5cf288871dca7542a45bfe8432313e55f18c22
Tomas Breuckmann <tomas.yoga@yahoo.com>
2021-12-30 14:52:45 -0300

    project meme generator - criterio 7 - npm test ok - projeto 100%

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-const fire = document.getElementById('fire')
+// const fire = document.getElementById('fire')
 const memeImageContainer = document.getElementById('meme-image-container')
+const meme1 = document.getElementById('meme-1')
+const meme2 = document.getElementById('meme-2')
+const meme3 = document.getElementById('meme-3')
+const meme4 = document.getElementById('meme-4')
+
+function adicionaImagem(event){
+    document.getElementById('meme-image').src = event.target.src
+}
+meme1.addEventListener('click',adicionaImagem)
+meme2.addEventListener('click',adicionaImagem)
+meme3.addEventListener('click',adicionaImagem)
+meme4.addEventListener('click',adicionaImagem)
 
 function memeText(){
     let texto=document.getElementById('text-input')
@@ -16,3 +28,4 @@ function bordaWater(){
 function bordaEarth(){
     memeImageContainer.style.border='6px groove green'
 }
+
